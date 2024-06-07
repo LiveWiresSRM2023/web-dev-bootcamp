06327eb89f313b0a6cc345d4e77c8b3375a6bbc6
Mohammed Vaseem <138885639+vaseem15905@users.noreply.github.com>
2024-06-07 15:08:25 +0530

    Update firebase.jsx

--- day-5/src/components/firebase.jsx
+++ day-5/src/components/firebase.jsx
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAf2CcNgsUDTpPOYpNPqg3CPraLdHAyNIE",
-  authDomain: "todo-db672.firebaseapp.com",
-  projectId: "todo-db672",
-  storageBucket: "todo-db672.appspot.com",
-  messagingSenderId: "101819614056",
-  appId: "1:101819614056:web:fc84c275909a06e6a08cdd"
+// Add Your API key
 };
 
 // Initialize Firebase
